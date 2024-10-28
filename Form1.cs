0d8651f27ddaa3ab729c71428f3b41ad0a563a3c
garces <guilherme.almada@uvvnet.com.br>
2024-10-27 21:39:05 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Agenda_Telefonica_Agenda_Sobre_Tela
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
